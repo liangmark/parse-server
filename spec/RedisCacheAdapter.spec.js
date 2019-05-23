00ccfaa8c7c72da044cdc556081e16fd7a29e16a
Diamond Lewis <findlewis@gmail.com>
2019-05-23 16:11:48 -0500

    put test

--- spec/RedisCacheAdapter.spec.js
+++ spec/RedisCacheAdapter.spec.js
@@ -109,43 +109,56 @@ describe_only(() => {
       enableSingleSchemaCache: true,
     });
     await cacheAdapter.clear();
-    const spy = spyOn(cacheAdapter, 'get').and.callThrough();
+    const getSpy = spyOn(cacheAdapter, 'get').and.callThrough();
+    const putSpy = spyOn(cacheAdapter, 'put').and.callThrough();
 
     // New Object
     const object = new TestObject();
     object.set('foo', 'bar');
     await object.save();
-    expect(spy.calls.count()).toBe(18);
-    spy.calls.reset();
+    expect(getSpy.calls.count()).toBe(18);
+    expect(putSpy.calls.count()).toBe(8);
+    getSpy.calls.reset();
+    putSpy.calls.reset();
 
     // Update Existing Field
     object.set('foo', 'barz');
     await object.save();
-    expect(spy.calls.count()).toBe(5);
-    spy.calls.reset();
+    expect(getSpy.calls.count()).toBe(5);
+    expect(putSpy.calls.count()).toBe(0);
+    getSpy.calls.reset();
+    putSpy.calls.reset();
 
     // Add New Field
     object.set('new', 'barz');
     await object.save();
-    expect(spy.calls.count()).toBe(11);
-    spy.calls.reset();
+    expect(getSpy.calls.count()).toBe(11);
+    expect(putSpy.calls.count()).toBe(4);
+    getSpy.calls.reset();
+    putSpy.calls.reset();
 
     // Get Object
     let query = new Parse.Query(TestObject);
     await query.get(object.id);
-    expect(spy.calls.count()).toBe(3);
-    spy.calls.reset();
+    expect(getSpy.calls.count()).toBe(3);
+    expect(putSpy.calls.count()).toBe(2);
+    getSpy.calls.reset();
+    putSpy.calls.reset();
 
     // Find Object
     query = new Parse.Query(TestObject);
     await query.find();
-    expect(spy.calls.count()).toBe(2);
-    spy.calls.reset();
+    expect(getSpy.calls.count()).toBe(2);
+    expect(putSpy.calls.count()).toBe(0);
+    getSpy.calls.reset();
+    putSpy.calls.reset();
 
     // Delete Object
     await object.destroy();
-    expect(spy.calls.count()).toBe(3);
-    spy.calls.reset();
+    expect(getSpy.calls.count()).toBe(3);
+    expect(putSpy.calls.count()).toBe(0);
+    getSpy.calls.reset();
+    putSpy.calls.reset();
 
     const objects = [];
     for (let i = 0; i < 100; i++) {
@@ -154,8 +167,10 @@ describe_only(() => {
       objects.push(obj);
     }
     await Parse.Object.saveAll(objects);
-    expect(spy.calls.count()).toBe(324);
-    spy.calls.reset();
+    expect(getSpy.calls.count()).toBe(324);
+    expect(putSpy.calls.count()).toBe(44);
+    getSpy.calls.reset();
+    putSpy.calls.reset();
 
     await cacheAdapter.clear();
   });
